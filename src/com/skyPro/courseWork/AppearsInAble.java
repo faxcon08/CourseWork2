ab6d0f140d4b1bb1a7f8cda98b23e14dfb9d1ef2
faxcon08 <faxcon-08@mail.ru>
2023-03-04 00:36:46 +0300

    ver 1.1 (изменил сравнение дат)

--- src/com/skyPro/courseWork/AppearsInAble.java
+++ src/com/skyPro/courseWork/AppearsInAble.java
@@ -8,17 +8,7 @@ public interface AppearsInAble {
 
     default  boolean isPastDate(LocalDateTime taskDateTime, LocalDate date){/// 02-03-23 & 01-01-24
         LocalDate taskDate= taskDateTime.toLocalDate();
-        if (taskDate.getYear() <= date.getYear()) { //23 23
-            if (taskDate.getYear() == date.getYear()) {
-                if (taskDate.getMonthValue() <= date.getMonthValue()) {
-                    if (taskDate.getMonthValue() == date.getMonthValue()) {
-                        if (taskDate.getDayOfMonth() <= date.getDayOfMonth()) {
-                            return false;
-                        }else return true;
-                    } else return false;
-                }else return true;
-            }else return false;
-        }
-        return true;
+
+        return date.isBefore(taskDate) && !date.isEqual(taskDate);
     }
 }
